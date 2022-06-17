da0d6bb75564814b33e9fc9240654bfd36e36951
daisvke <daisuketanigawa@live.fr>
2022-06-17 04:25:13 +0200

    Set mysqli charset to utf

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -88,10 +88,6 @@ function Confirmation() {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-const header = {
-    'Content-Type': 'charset=utf-8'
-}
-
     axios.post('http://127.0.0.1:8888/backend/index.php', {
       data: {
         language: language,
@@ -114,7 +110,6 @@ const header = {
         cbExpDate: cbExpDate,
         cbSecurityCode: cbSecurityCode,
       },
-      headers: header
     })
     dispatch({
       type: 'setEnv',
@@ -129,7 +124,7 @@ const header = {
 
   return (
     <>
-      {(
+      {submitted === false && (
         <form onSubmit={handleSubmit}>
           <ConfirmSheet>
             <p>Please verify your inputs and confirm the check-in</p>
